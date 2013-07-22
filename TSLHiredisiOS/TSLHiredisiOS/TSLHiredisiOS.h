7e7c32a8758fcceaf8456a0ecf6fcff1a94254df
Deepak Natarajan <dnataraj@trilobytesystems.com>
2013-07-22 14:23:06 +0300

    Added Hiredis ARM6/7 projects to workspace

---
+++ TSLHiredisiOS/TSLHiredisiOS/TSLHiredisiOS.h
@@ -0,0 +1,13 @@
+//
+//  TSLHiredisiOS.h
+//  TSLHiredisiOS
+//
+//  Created by Deepak Natarajan on 7/22/13.
+//  Copyright (c) 2013 Trilobyte Systems ApS. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TSLHiredisiOS : NSObject
+
+@end
---
+++ TSLHiredisiOS/TSLHiredisiOS/TSLHiredisiOS.m
@@ -0,0 +1,13 @@
+//
+//  TSLHiredisiOS.m
+//  TSLHiredisiOS
+//
+//  Created by Deepak Natarajan on 7/22/13.
+//  Copyright (c) 2013 Trilobyte Systems ApS. All rights reserved.
+//
+
+#import "TSLHiredisiOS.h"
+
+@implementation TSLHiredisiOS
+
+@end
